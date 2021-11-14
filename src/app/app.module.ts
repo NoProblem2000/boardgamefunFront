a621fa5359d90447fec72e906532d28fe58c0ea6
i.i.artamonov <fernando94@yandex.ru>
2021-11-14 11:37:20 +0400

    Header and main draft components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {RouterModule, Routes} from "@angular/router";
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './layout/header/header.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MainPageComponent} from "./layout/main/main-page.component";
+
+const routes: Routes =[
+  { path: '', component: MainPageComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
