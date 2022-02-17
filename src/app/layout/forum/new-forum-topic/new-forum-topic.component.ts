cbf5baa0bca96c0f37e9d5c6c4d33d63bc3afc67
i.i.artamonov <fernando94@yandex.ru>
2022-02-17 14:41:50 +0400

    Add layout to NewForumTopic component

--- src/app/layout/forum/new-forum-topic/new-forum-topic.component.ts
+++ src/app/layout/forum/new-forum-topic/new-forum-topic.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {AbstractControl, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-new-forum-topic',
@@ -7,13 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewForumTopicComponent implements OnInit {
 
-
+  gameId: number;
   title!: string;
   text!: string;
+  messageFormGroup!: FormGroup;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,) {
+    this.gameId = Number(this.route.snapshot.paramMap.get('id'));
+  }
 
   ngOnInit(): void {
+    this.messageFormGroup = new FormGroup({
+      title: new FormControl(this.title, [Validators.required]),
+      text: new FormControl(this.text, [Validators.required])
+    })
+  }
+
+  get getTitle(): AbstractControl | null{
+    return this.messageFormGroup.get('title');
+  }
+
+  get getText(): AbstractControl | null{
+    return this.messageFormGroup.get('text');
+  }
+
+  onSubmit(): void{
+    if (this.messageFormGroup.valid){
+      this.title = this.getTitle?.value;
+      this.text = this.getText?.value;
+      this.createNewTopic()
+    } else{
+      this.messageFormGroup.reset();
+    }
+  }
+
+  createNewTopic(){
   }
 
 }
